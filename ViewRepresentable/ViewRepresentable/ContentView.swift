42d7c6b4fdc9e6bf1388d8aa1669ed39e9313b6a
Nitin <nbnitin.bhatia@gmail.com>
2022-09-30 11:40:43 +0530

    View Representable project added...

---
+++ ViewRepresentable/ViewRepresentable/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  ViewRepresentable
+//
+//  Created by Nitin Bhatia on 28/09/22.
+//
+
+import SwiftUI
+
+struct TextView: UIViewRepresentable {
+    @Binding var text: String
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIView(context: Context) -> UITextView {
+
+        let myTextView = UITextView()
+        myTextView.delegate = context.coordinator
+
+        myTextView.font = UIFont(name: "HelveticaNeue", size: 15)
+        myTextView.isScrollEnabled = true
+        myTextView.isEditable = true
+        myTextView.isUserInteractionEnabled = true
+        myTextView.backgroundColor = UIColor(white: 0.0, alpha: 0.05)
+
+        return myTextView
+    }
+
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        uiView.text = text
+    }
+
+    class Coordinator : NSObject, UITextViewDelegate {
+
+        var parent: TextView
+
+        init(_ uiTextView: TextView) {
+            self.parent = uiTextView
+        }
+
+        func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+            return true
+        }
+
+        func textViewDidChange(_ textView: UITextView) {
+            print("text now: \(String(describing: textView.text!))")
+            self.parent.text = textView.text
+        }
+    }
+}
+
+
+struct ContentView: View {
+    @State private var text = ""
+    @State private var textChange = false
+    var body: some View {
+        VStack {
+            TextView(text: $text)
+               
+                
+            if textChange {
+                ProgressView()
+            }
+        }
+        
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
