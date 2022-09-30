245aad8a0b5b7a9e8a23db455c43d092be7c1daf
Nitin <nbnitin.bhatia@gmail.com>
2022-09-30 11:40:43 +0530

    View Representable project added...

---
+++ ViewRepresentable/ViewRepresentable/ViewRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewRepresentableApp.swift
+//  ViewRepresentable
+//
+//  Created by Nitin Bhatia on 28/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
