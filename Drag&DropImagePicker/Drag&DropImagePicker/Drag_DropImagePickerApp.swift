e9b10b0bf18ddb4d1aa4f1377fd92377f7a7b72c
Nitin <nbnitin.bhatia@gmail.com>
2023-08-28 11:44:33 +0530

    New drag drop project demo added...

---
+++ Drag&DropImagePicker/Drag&DropImagePicker/Drag_DropImagePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drag_DropImagePickerApp.swift
+//  Drag&DropImagePicker
+//
+//  Created by Nitin Bhatia on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Drag_DropImagePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
