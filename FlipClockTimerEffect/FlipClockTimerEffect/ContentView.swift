cfc59f186d0114b326b6c0c725c93464a309c087
Nitin <nbnitin.bhatia@gmail.com>
2024-05-30 17:06:44 +0530

    minor change...

--- FlipClockTimerEffect/FlipClockTimerEffect/ContentView.swift
+++ FlipClockTimerEffect/FlipClockTimerEffect/ContentView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var timer : CGFloat = 0
+    @State private var timer = Timer.publish(every: 0.01, on: .current, in: .common).autoconnect()
+    @State private var timerCount : CGFloat = 0
     @State private var count : Int = 0
     var body: some View {
         NavigationStack {
@@ -17,13 +18,20 @@ struct ContentView: View {
                     FlipClockTimerView(value: .constant(count / 10), size: CGSize(width: 100, height: 150), fontSize: 70, foreground: .white, background: .red, cornerRadius: 20)
                     FlipClockTimerView(value: .constant(count % 10), size: CGSize(width: 100, height: 150), fontSize: 70, foreground: .white, background: .red, cornerRadius: 20)
                 })
+                Text("Timer for 20 seconds")
+                    .padding(.top)
             }
             .padding()
-            .onReceive(Timer.publish(every: 0.01, on: .current, in: .common).autoconnect(), perform: { _ in
-                timer += 0.01
+            
+            .onReceive(timer, perform: { _ in
+                timerCount += 0.01
                 
-                if timer >= 60 {timer = 0}
-                count = Int(timer)
+                if timerCount >= 60 {timerCount = 0}
+                count = Int(timerCount)
+                
+                if count / 10 == 20 {
+                    timer.upstream.connect().cancel()
+                }
             })
         }
     }
