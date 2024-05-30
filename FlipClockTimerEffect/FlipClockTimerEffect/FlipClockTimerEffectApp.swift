60e9c89ebbb7a61a9d080aa8f56cb6531ef10db9
Nitin <nbnitin.bhatia@gmail.com>
2024-05-30 16:56:56 +0530

    new clock effect project added...

---
+++ FlipClockTimerEffect/FlipClockTimerEffect/FlipClockTimerEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipClockTimerEffectApp.swift
+//  FlipClockTimerEffect
+//
+//  Created by Nitin Bhatia on 30/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FlipClockTimerEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
