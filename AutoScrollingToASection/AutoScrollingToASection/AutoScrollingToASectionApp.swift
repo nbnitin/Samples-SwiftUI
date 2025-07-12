1b4f9cc521481f3086a0050b1edc03ff19147a32
Nitin Bhatia <nbnitin.bhatia@gmail.com>
2025-07-12 21:38:55 +0530

    Add files via upload

---
+++ AutoScrollingToASection/AutoScrollingToASection/AutoScrollingToASectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoScrollingToASectionApp.swift
+//  AutoScrollingToASection
+//
+//  Created by Nitin Bhatia on 12/07/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoScrollingToASectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoScrollingToASection/AutoScrollingToASection/TabBarNavExample/SectionListForTabView.swift
@@ -0,0 +1,58 @@
+//
+//  SectionList.swift
+//  AutoScrollingToASection
+//
+//  Created by Nitin Bhatia on 12/07/25.
+//
+
+import SwiftUI
+
+struct SectionListForTabView: View {
+    let sectionId: Int
+    var body: some View {
+        NavigationView {
+            ScrollViewReader { scroll in
+                List(0...2, id: \.self) {index in
+                    let uniqueId = "section-\(index)"
+                    Section {
+                        ForEach(0...20, id: \.self) { rowIndex in
+                            Text("Section \(index) Item \(rowIndex + 1)")
+                                .listRowInsets(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 16))
+                                .listRowSeparatorTint(.blue)
+                                .alignmentGuide(.listRowSeparatorLeading) { d in
+                                    return -30 // Adjust the inset here
+                                }
+                        }
+                        
+                    } header: {
+                        Text("Section Header \(index)")
+                            .font(.headline)
+                            .padding([.leading, .bottom, .top], 20)
+                            .frame(maxWidth:.infinity, alignment: .leading)
+                            .background(Color.gray.opacity(0.1))
+                        
+                    }
+                    .listSectionSeparator(.hidden, edges: .top) // Remove top separator
+                    .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    .id(uniqueId)
+                    .onAppear {
+                        print(sectionId)
+                        withAnimation{
+                            scroll.scrollTo("section-\(sectionId)", anchor: .topLeading)
+                        }
+                    }
+                }
+                .listStyle(.plain) //sticky header
+                .listRowSpacing(20)
+                .padding(.all, 0)
+            }
+            .navigationTitle("All Sections")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        
+    }
+}
+
+#Preview {
+    TabsView()
+}
