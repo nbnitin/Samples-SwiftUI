52edf94682d016cfd7fc2e982d982d3a10b51378
Nitin Bhatia <nbnitin.bhatia@gmail.com>
2025-11-04 09:58:41 +0530

    Add files via upload

---
+++ UpdateChecker/UpdateCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpdateCheckerApp.swift
+//  UpdateChecker
+//
+//  Created by Nitin Bhatia on 01/11/25.
+//
+
+import SwiftUI
+
+@main
+struct UpdateCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
